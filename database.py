b176d60fef4f85bd6b96805cbf4eb4437c145cb7
vamsi11126 <vamsimanukinda26@gmail.com>
2025-10-19 10:07:46 +0530

    modified the url

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-engine = create_engine(r"sqlite:///C:/Users/vamsi/Desktop/FastAPI/BLog/blog.db", connect_args={"check_same_thread": False})
+import os
+db_path = "/tmp/blog.db" if os.getenv("RENDER") else "./blog.db"
 
+engine = create_engine(f"sqlite:///{db_path}", connect_args={"check_same_thread": False})
 Base=declarative_base()
 
 sessionLocal=sessionmaker(bind=engine,autoflush=False,autocommit=False)
